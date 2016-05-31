7afd92e92c0ea7ba628d627f426afe2e0ded1250
chaggay <chaggay@hotmail.com>
2016-06-01 02:08:43 +0300

    setting image and url with object

--- www/js/controllers/game.controller.js
+++ www/js/controllers/game.controller.js
@@ -12,15 +12,7 @@
         activate();
 
         function activate() {
-            $scope.image = {
-                url: 'http://myminidisco.co.uk/wp-content/uploads/2012/04/blue-box1.png',
-                background: 'orange'
-            }
-
-            $scope.image2 = {
-                url: 'http://reddeadredemptionps3.webstarts.com/uploads/right_red_box.jpg',
-                background: 'black'
-            }
+            
         }
     }
 })();
